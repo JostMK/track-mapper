ce96c127933746b234fcb1002915f23ef95cc8d9
JostMK <jost.klink@gmail.com>
2024-07-09 18:32:39 +0200

    Refactor project structure
    renamed src/graphs to src/graph
    split cmake project into subdirectories, see src\graph\CMakeLists.txt and src\web\CMakeLists.txt
    split graph logic into shared TrackMapperGraphLib and standalone TrackMapperGraphConsoleApp
    split web logic into shared TrackMapperServerLib and standalone TrackMapperServerWebApp
    moved main.cpp functionality into TrackMapperGraphConsoleApp

--- src/graph/main.cpp
+++ src/graph/main.cpp
@@ -1,53 +1,51 @@
 
 #include <chrono>
 #include <iostream>
-#include "src/graphs/FMIGraphReader.h"
-#include "src/graphs/DijkstraPathfinding.h"
-#include "src/graphs/FMIGraphReader.h"
-#include "src/graphs/DijkstraPathfinding.h"
-#include "src/web/TestWebApp.h"
 
-static void TestWebApp();
-static void TestGraphApp();
-static void PrintGraph(const BasicGraph& graph);
-static void QueryGraph(const BasicGraph& graph);
-static void QueryShortestPath(const BasicGraph& graph);
+#include "BasicGraph.h"
+#include "DijkstraPathfinding.h"
+#include "FMIGraphreader.h"
 
-int main()
-{
-    //TestGraphApp();
-    TestWebApp();
-    return 0;
-}
+void PrintGraph(const BasicGraph &graph);
 
-void TestWebApp() {
+void QueryGraph(const BasicGraph &graph);
+
+void QueryShortestPath(const BasicGraph &graph);
+
+int main() {
     std::cout << "Enter Path to fmi file:" << std::endl;
 
     std::string filePath;
-    std::cin>>filePath;
+    std::cin >> filePath;
     const BasicGraph graph = FMIGraphReader::read(filePath);
 
-    TestWebApp::Start(graph);
+    bool run = true;
+    while (run) {
+        std::cout << "Options: Print Graph (g); Query Graph Node (n); Query Shortest Path (p); Quit (q)" << std::endl;
+        std::string option;
+        std::cin >> option;
+
+        switch (option[0]) {
+            case 'q': run = false;
+                break;
+            case 'g': PrintGraph(graph);
+                break;
+            case 'n': QueryGraph(graph);
+                break;
+            case 'p': QueryShortestPath(graph);
+                break;
+            default: std::cout << "Use one of the options: " << std::endl;
+                break;
+        }
+    }
 }
 
-
-static void TestGraphApp() {
-    std::cout << "Enter Path to fmi file:" << std::endl;
-
-    std::string filePath;
-    std::cin>>filePath;
-    const BasicGraph graph = FMIGraphReader::read(filePath);
-
-    QueryShortestPath(graph);
-}
-
-
-static void PrintGraph(const BasicGraph& graph){
+void PrintGraph(const BasicGraph &graph) {
     for (int i = 0; i < graph.GetNodeCount(); ++i) {
         auto [latitude, longitude] = graph.GetLocation(i);
         std::cout << "Location " << i << ": " << latitude << " : " << longitude << std::endl;
 
-        if(auto e = graph.GetEdges(i); !e.empty()) {
+        if (auto e = graph.GetEdges(i); !e.empty()) {
             for (auto [adjacentNodeIndex, distance]: e) {
                 std::cout << " > Edge " << i << " -> " << adjacentNodeIndex << " : " << distance << std::endl;
             }
@@ -55,18 +53,18 @@ static void PrintGraph(const BasicGraph& graph){
     }
 }
 
-static void QueryGraph(const BasicGraph& graph){
-    while(true) {
+void QueryGraph(const BasicGraph &graph) {
+    while (true) {
         std::cout << "Enter node id or enter -1 to exit:" << std::endl;
         int nodeIndex;
         std::cin >> nodeIndex;
 
-        if(nodeIndex < 0)
+        if (nodeIndex < 0)
             break;
 
         auto [lat, lon] = graph.GetLocation(nodeIndex);
         std::cout << "Node " << nodeIndex << ": " << lat << " ; " << lon << std::endl;
-        if(auto e = graph.GetEdges(nodeIndex); !e.empty()) {
+        if (auto e = graph.GetEdges(nodeIndex); !e.empty()) {
             for (auto [adjacentNodeIndex, distance]: e) {
                 std::cout << " > Edge " << nodeIndex << " -> " << adjacentNodeIndex << " : " << distance << std::endl;
             }
@@ -74,22 +72,22 @@ static void QueryGraph(const BasicGraph& graph){
     }
 }
 
-static void QueryShortestPath(const BasicGraph& graph){
+void QueryShortestPath(const BasicGraph &graph) {
     const DijkstraPathfinding dijkstra(graph);
 
-    while(true) {
+    while (true) {
         std::cout << "Enter start node id or enter -1 to exit:" << std::endl;
         int startNodeIndex;
         std::cin >> startNodeIndex;
 
-        if(startNodeIndex < 0)
+        if (startNodeIndex < 0)
             break;
 
         std::cout << "Enter target node id or enter -1 to exit:" << std::endl;
         int targetNodeIndex;
         std::cin >> targetNodeIndex;
 
-        if(targetNodeIndex < 0)
+        if (targetNodeIndex < 0)
             break;
 
         auto startTime = std::chrono::high_resolution_clock::now();
---
+++ src/web/main.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Jost on 24/05/2024.
+//
+
+#include <iostream>
+
+#include "TestWebApp.h"
+#include "../graph/FMIGraphreader.h"
+#include "../graph/BasicGraph.h"
+
+void TestWebApp();
+
+int main()
+{
+    TestWebApp();
+    return 0;
+}
+
+void TestWebApp() {
+    std::cout << "Enter Path to fmi file:" << std::endl;
+
+    std::string filePath;
+    std::cin>>filePath;
+    const BasicGraph graph = FMIGraphReader::read(filePath);
+
+    TestWebApp::Start(graph);
+}
