4a307373f38059c1e9fe49db0f79b0933c26dcde
JostMK <jost.klink@gmail.com>
2024-09-10 22:30:55 +0200

    Extends fbx converter to allow selecting output format

--- experiments/fbx-export/convertfbx.cpp
+++ experiments/fbx-export/convertfbx.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <fbxsdk.h>
+#include <iostream>
 
 int main() {
     std::cout << "Enter path to fbx file: " << std::endl;
@@ -22,10 +22,21 @@ int main() {
     lImporter->Import(lScene);
     lImporter->Destroy();
 
+    std::cout << "Export as [a]scii or [b]inary format: " << std::endl;
+    std::string formatOption;
+    std::cin >> formatOption;
+
+    int format;
+    if (formatOption[0] == 'a') {
+        format = lSdkManager->GetIOPluginRegistry()->FindWriterIDByDescription("FBX ascii (*.fbx)");
+    } else {
+        format = lSdkManager->GetIOPluginRegistry()->FindWriterIDByDescription("FBX binary (*.fbx)");
+    }
+
     const auto lExporter = FbxExporter::Create(lSdkManager, "");
-    const int lAsciiFormat = lSdkManager->GetIOPluginRegistry()->FindWriterIDByDescription("FBX ascii (*.fbx)");
-    const std::string outfileName = lFilename.substr(0,lFilename.size()-4) + "-ascii.fbx";
-    if(const bool lExportStatus = lExporter->Initialize(outfileName.c_str(), lAsciiFormat, lSdkManager->GetIOSettings()); !lExportStatus) {
+    const std::string outfileName = lFilename.substr(0, lFilename.size() - 4) + "-converted.fbx";
+    if (const bool lExportStatus = lExporter->Initialize(outfileName.c_str(), format, lSdkManager->GetIOSettings());
+        !lExportStatus) {
         printf("Call to FbxExporter::Initialize() failed.\n");
         printf("Error returned: %s\n\n", lExporter->GetStatus().GetErrorString());
     }
@@ -37,4 +48,3 @@ int main() {
     lSdkManager->Destroy();
     return 0;
 }
-
